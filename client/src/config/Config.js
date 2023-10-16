b67cd2314dde3ed8f08d64880d1e8ae80d474cbf
omar kanaan <93411313+0m4rk4n4n@users.noreply.github.com>
2023-10-15 18:15:03 -0600

    Update Config.js

--- client/src/config/Config.js
+++ client/src/config/Config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 export const axiosInstance=axios.create(
     {
-        baseURL:"https://buyandsell-m45c.onrender.com/api/"
+        baseURL:"https://buyandsell-ttul.onrender.com/api/"
     })
