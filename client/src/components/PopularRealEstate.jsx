0cf1eee7cc6934bf8b2daa81df8a44062ff0c9d1
omar kanaan <93411313+0m4rk4n4n@users.noreply.github.com>
2023-10-20 15:16:44 -0600

    Update PopularRealEstate.jsx

--- client/src/components/PopularRealEstate.jsx
+++ client/src/components/PopularRealEstate.jsx
@@ -8,14 +8,12 @@ const PopularRealEstate=()=>
 return(
 <><div className="showHeadersOnMobile" style={{display:"flex",alignItems:"center",justifyContent:"space-between",padding:25,display:"none"}}>
 <div className="homeHeadersRealEstate" style={{fontWeight:"bold"}}>Popular in Real Estate</div>
-<Link to="real-estate/ads" className="homeHeadersRealEstate">Browse All Real Estate</Link>
 </div>
 <div className="container">
 <div style={{display:"block"}} className="row">
 <div className="col-lg-12 popular">
 <div className="hideHeadersOnMobile" style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
 <div style={{fontWeight:"bold",padding:"20px 0px"}}>Popular in Real Estate</div>
-<div style={{padding:"20px 70px"}}>Browse All Real Estate</div>
 </div>
 <div className="cards">
 <Link to="/real-estate/Condos-For-Sale" style={{backgroundImage:`url(${Apartments})`,backgroundPosition:"center"}} className="realestateCard">
