a5aa1f48fb0fea41ee0481f500c2e5100e1467f1
omar kanaan <93411313+0m4rk4n4n@users.noreply.github.com>
2023-10-23 05:43:46 -0600

    Update SignInToSeeMore.jsx

--- client/src/components/SignInToSeeMore.jsx
+++ client/src/components/SignInToSeeMore.jsx
@@ -8,7 +8,6 @@ return(<>
         <div className="col-lg-8 effects">
 <h5 style={{textAlign:"center",color:"#fff",padding:35}}>Buy & Sell is a lot better when you’re a member</h5>
 <p style={{textAlign:"center",color:"#fff"}}>See more relevant listings, find what you’re looking for quicker, and more!</p>
-                <br/>
 <button className="SignInToSeeMoreBtn" style={{display:"block",cursor:"pointer",margin:"30px auto",borderRadius:5,padding:5,borderColor:"transparent"}}><Link style={{textDecoration:"none",color:"inherit"}} to="/login">Sign In</Link></button>
         </div>
     </div>
