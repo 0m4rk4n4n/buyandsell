a8a1ada66eb0288335120604958930f4c243ab5c
omar kanaan <93411313+0m4rk4n4n@users.noreply.github.com>
2023-10-24 05:43:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 
 const io = socketIo(server, {
   cors: {
-    origin: "https://buyandsell.vip"
+    origin: "https://sellsphere.online"
   },
 });
 
