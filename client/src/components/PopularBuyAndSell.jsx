71b2e42732c4af06d520702bf914ce64f74d5abe
omar kanaan <93411313+0m4rk4n4n@users.noreply.github.com>
2023-10-24 05:39:19 -0600

    Update PopularBuyAndSell.jsx

--- client/src/components/PopularBuyAndSell.jsx
+++ client/src/components/PopularBuyAndSell.jsx
@@ -19,13 +19,13 @@ useEffect(()=>
 return(
 <>
 <div className="showHeadersOnMobile" style={{display:"flex",alignItems:"center",justifyContent:"space-between",padding:25,display:"none"}}>
-            <div className="homeHeadersbuyandsell" style={{fontWeight:"bold"}}>Popular in Buy and Sell</div>
+            <div className="homeHeadersbuyandsell" style={{fontWeight:"bold"}}>Popular in Sellsphere</div>
             </div>
 <div className="container">
     <div style={{display:"block"}} className="row">
         <div className="col-lg-12 popular">
         <div className="hideHeadersOnMobile" style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
-            <div style={{fontWeight:"bold",padding:"20px 0px"}}>Popular in Buy and Sell</div>
+            <div style={{fontWeight:"bold",padding:"20px 0px"}}>Popular in Sellsphere</div>
             </div>
 <div className="cards">
     <Link to="/jobs/jobs" style={{backgroundImage:`url(${Furniture})`,backgroundPosition:"center"}} className="buyandsellCard">
