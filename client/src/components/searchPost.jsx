f36866cd5cd23688db0fee85892af0c1bc5e013b
omar kanaan <93411313+0m4rk4n4n@users.noreply.github.com>
2023-11-08 23:36:54 -0700

    Update searchPost.jsx

--- client/src/components/searchPost.jsx
+++ client/src/components/searchPost.jsx
@@ -11,15 +11,16 @@ return(<>
         </div>
         <div style={{width:"100%"}} className="cardDetails">
         <div style={{display:"flex",alignItems:"center",justifyContent:"space-between"}}>
-        <div className="" style={{fontWeight:"bold",color:"#373373",fontSize:18}}>{ad.title[0].toUpperCase() + ad.title.slice(1).toLowerCase()}</div>
-        <div className="priceForMobile" style={{color:"green",fontWeight:"bold",fontSize:20}}>{"$"}{ad.price.toLocaleString() && ad.price.toLocaleString()}</div>
+        <div style={{fontWeight:"bold",color:"#373373",fontSize:18}}>{ad.title[0].toUpperCase() + ad.title.slice(1).toLowerCase()}</div>
+        {ad.adType==="job" ? <div className="priceForMobile salaryforMobile" style={{color:"green",fontWeight:"bold",fontSize:20}}>{"$"}{ad.salary===45000 ? "40,000 - 50,000 / Year" : ad.salary===55000 ? "50,000 - 60,000 / Year" : ad.salary===65000 ? "60,000 - 70,000 / Year" : ad.salary===75000 ? "70,000 - 80,000 / Year" : ad.salary===85000 ? "80,000 - 90,000 / Year" : ad.salary===95000 ? "90,000 - 100,000 / Year" : "100,000 / Year"}</div> :  <div className="priceForMobile" style={{color:"green",fontWeight:"bold",fontSize:20}}>{"$"}{ad.price?.toLocaleString() && ad.price.toLocaleString()}</div>}
         </div>
-
+       <div style={{height:3}}></div>
         <div style={{display:"flex",alignItems:"center",gap:10}}>
-            <div className="location">{ad.location}</div>
+            <div className="location">{ad.location==="Newfoundland and Labrador" ? "Newfoundland" : ad.location}</div>
             <div>|</div>
             <div>{format(ad.createdAt)}</div>
         </div>
+        <div style={{height:3}}></div>
         <div className="desc paddingOff">
         {ad.description.length>180 ? ad.description.slice(0,180)+"..." : ad.description}
             </div>
